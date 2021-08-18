0b1d189e9d89f8df2497a68f3424e788fc740639
Edgar Cortés <edgar.cortesortega@igt.com>
2021-08-18 15:35:14 -0500

    adding pending files with ngif and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-store';
+
+  items = ['edgar', 'diego', 'denise'];
+
+  addItem() {
+    this.items.push('Nuevo Item');
+  }
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
